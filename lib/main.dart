e1c3303ea3d32fa87d2c0008ad514143d96fe53c
zimin <wlals822@naver.com>
2020-05-31 06:15:42 +0900

    앱 이름 관련 클래스 변경

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(BoardGamersApp());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class BoardGamersApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'BoadGamers',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: InitialPage(title: 'Flutter Demo Home Page'),
+      home: InitialPage(),
     );
   }
 }
 
 class InitialPage extends StatefulWidget {
-  InitialPage({Key key, this.title}) : super(key: key);
+  InitialPage({Key key}) : super(key: key);
 
-  final String title;
+  final String title = "BoardGamers";
 
   @override
   _InitialPageState createState() => _InitialPageState();
@@ -38,16 +37,8 @@ class _InitialPageState extends State<InitialPage> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-          ],
+          children: <Widget>[],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(Icons.add),
       ),
     );
   }
